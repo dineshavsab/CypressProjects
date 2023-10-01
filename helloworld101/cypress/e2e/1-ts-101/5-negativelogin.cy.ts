cf22fac8cf3688b30195c84d40e0125000d20a62
vsdinesha <dineshavs@hotmail.com>
2023-10-01 09:38:57 -0400

    Added Negative tcs

--- helloworld101/cypress/e2e/1-ts-101/5-negativelogin.cy.ts
+++ helloworld101/cypress/e2e/1-ts-101/5-negativelogin.cy.ts
@@ -4,9 +4,9 @@ it('Enters a value for User Id', () =>
     cy.get('[data-cy="userid"]')
         .type('test')
     cy.get('[data-cy="password"]')
-        .type('test')
+        .type('testing')
     cy.get('[data-cy="submit"]')
         .click()            
 
-    cy.url().should('eq', `${Cypress.config().baseUrl}homepage` )
+    cy.url().should('eq', `${Cypress.config().baseUrl}unauthorized` )
 })
