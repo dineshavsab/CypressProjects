ac9eaa7e4da252137ccb2d5c086287314f8b5edd
vsdinesha <dineshavs@hotmail.com>
2023-10-03 07:13:25 -0400

    Exposing JSON data as Fixtures with typesafety

--- helloworld101/cypress/e2e/1-ts-101/8-login-using-fixture.cy.ts
+++ helloworld101/cypress/e2e/1-ts-101/8-login-using-fixture.cy.ts
@@ -1,13 +1,29 @@
-import * as userData from "../../fixtures/userData.json"
+
+import type {userData } from '../../fixtures/userdata.model'
+
 it('Loads data from Fixture to test scenarios', () =>
 {
-    cy.fixture('userData.json')
-    .its('userData')
+    // Define what type of data can you expect from json
+    // in this case it's an array of userData
+    // This will come in very handy to avoid any typos as you are coding
+    // and be typesafe !!!
+
+    //You need specify the property name you are interested 
+    // from the object that you are expecting in the its()
+
+    // in the then -  /* Again use the same name to chain the types through the block */
+    // Now if you accidentally type user.name or something else where a property is 
+    // not defined in the interface, typescript will throw exception
+    // That is SO NEAT !   
+
+    cy.fixture< {users : userData[]} >('userData.json')
+    .its('userData') 
     .should('be.an',  'array')
     .then( (users ) =>
     {
         users.forEach(user => {
-            cy.visit('login')
+            cy.visit('login')      
+               
             cy.getMyElementByName('userid').type(user.userid);
             cy.getMyElementByName('password').type(user.password);
             cy.getMyElementByName('submit').click()
---
+++ helloworld101/cypress/fixtures/userdata.model.ts
@@ -0,0 +1,5 @@
+export interface userData
+{
+    userid : string,
+    password : string
+}
